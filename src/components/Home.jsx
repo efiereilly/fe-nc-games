3469103649cb7cd28057578197af0b067f2f4601
Efie Reilly <efiereilly@efies-air-2.home>
2023-05-30 16:10:02 +0100

    updated ticket4

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,15 @@
 import { useEffect, useState } from "react";
 import { fetchAllReviews } from "../utils/utils";
-import "./Home.css";
+import "./home.css";
 
 const Home = () => {
 
-    const [allReviews, setAllReviews] = useState ([])
+    const [recentReviews, setRecentReviews] = useState ([])
     const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
         fetchAllReviews().then(({ reviews }) => {
-            setAllReviews(reviews)
+            setRecentReviews(reviews.slice(0,3))
             setIsLoading(false)
      }  ) 
     }, [])
@@ -21,7 +21,7 @@ const Home = () => {
     <p>
     {isLoading ? <p>Loading.. </p> :
             <ul>
-                    {allReviews.slice(0,3).map((review) => {
+                    {recentReviews.map((review) => {
                 return <li className="reviews" key={review.review_id}>
                 <h3>{review.title}</h3>
                 <p> Created at: {review.created_at.slice(0, 10)}</p>
